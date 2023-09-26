5e0e42b8363b1733d39b63116a141b1376c5122c
Agm3002 <104405812+Agm3002@users.noreply.github.com>
2023-09-26 11:07:54 -0700

    creacion de index

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Practica de clase 25/09/23")
+
+console.log("Integrantes:")
+console.log("Luis Daniel Quiroz Osuna")
+console.log("Armando Gonzalez Martinez")
+console.log("Andres Antelo Figueroa")
